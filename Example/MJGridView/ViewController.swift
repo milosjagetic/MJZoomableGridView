e5255fef2045a1d6db3ec5c7681108cddd7f3041
Miloš Jagetić <milos.jagetic@gmail.com>
2019-09-22 20:48:57 +0200

    example update

--- Example/MJGridView/ViewController.swift
+++ Example/MJGridView/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 import MJGridView
 
+private extension String
+{
+    static let placementIcons: String = "╋ ┳ ┓ ┫ ┛ ┻ ┖ ┣ ┏"
+}
+
 class ViewController: UIViewController
 {
     @IBOutlet weak var gridView: ScrollingGridView!
     
+    @IBOutlet weak var spacingLabel: UILabel!
+    @IBOutlet weak var placementLabel: UILabel!
+    @IBOutlet weak var spacingSlider: UISlider!
+    @IBOutlet weak var placementSlider: UISlider!
+    @IBOutlet weak var toolboxHidden: NSLayoutConstraint!
+
     
-    @IBAction func stepperChanged(_ stepper: UIStepper!)
+    override func viewDidLoad()
     {
-        let placements: [OriginPlacement] = [.center, .topCenter, .topRight, .centerRight, .bottomRight, .bottomCenter, .bottomLeft, .centerLeft, .topLeft]
-        gridView.gridView.originPlacement = placements[Int(stepper.value) % placements.count]
+        super.viewDidLoad()
+        
+        spacingSlider.minimumValue = 25
+        spacingSlider.maximumValue = 300
+        spacingSlider.value = 25
+        
+        placementSlider.minimumValue = 0
+        placementSlider.maximumValue = Float(OriginPlacement.allCases.count - 1)
+        placementSlider.value = 0
+        
+        spacingChanged(slider: spacingSlider)
+        originPlacementChanged(slider: placementSlider)
     }
 
+    
+    //  //= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\\
+    //  MARK: Actions -
+    //  \\= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =//
+    @IBAction func spacingChanged(slider: UISlider!)
+    {
+        gridView.gridView.pixelsPerLine = UInt(slider.value)
+        spacingLabel.text = "Spacing: \(gridView.gridView.pixelsPerLine)"
+    }
 
+    @IBAction func originPlacementChanged(slider: UISlider!)
+    {
+        let index: Int = Int(slider.value) % OriginPlacement.allCases.count
+        guard index != OriginPlacement.allCases.lastIndex(of: gridView.gridView.originPlacement) else {return}
+        
+        gridView.gridView.originPlacement = OriginPlacement.allCases[index]
+        placementLabel.text = "Placement: \(String.placementIcons.components(separatedBy: " ")[index])"
+    }
+    
+    @IBAction func toolboxButtonTapped(button: UIButton!)
+    {
+        button.isSelected = !button.isSelected
+        toolboxHidden.priority = button.isSelected ? .defaultLow : .defaultHigh
+        UIView.animate(withDuration: 1/4, delay: 0, usingSpringWithDamping: 4/5, initialSpringVelocity: 1, options: [], animations:
+        {
+            self.view.layoutIfNeeded()
+        })
+    }
 }
 
