da70a03850ce64a13805c7cf2da3af39fefc9750
Miloš Jagetić <milos.jagetic@gmail.com>
2020-02-02 20:00:41 +0100

    example app zoom stepper

--- Example/MJGridView/ViewController.swift
+++ Example/MJGridView/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController
     @IBOutlet weak var spacingSlider: UISlider!
     @IBOutlet weak var placementSlider: UISlider!
     @IBOutlet weak var toolboxHidden: NSLayoutConstraint!
+    @IBOutlet weak var zoomLabel: UILabel!
+    @IBOutlet weak var zoomStepper: UIStepper!
+
 
     
     override func viewDidLoad()
@@ -37,16 +40,22 @@ class ViewController: UIViewController
         placementSlider.maximumValue = Float(OriginPlacement.allCases.count - 1)
         placementSlider.value = 0
         
+        zoomStepper.value = 0
+        zoomStepper.minimumValue = log2(Double(gridView.minimumZoomScale))
+        zoomStepper.maximumValue = log2(Double(gridView.maximumZoomScale))
+        
+        stepperValueChanged(zoomStepper)
         spacingChanged(slider: spacingSlider)
         originPlacementChanged(slider: placementSlider)
         
         gridView.backgroundColor = UIColor(red: 73/255, green: 72/255, blue: 80/255, alpha: 1)
 
-//        gridView.gridProperties.lineColor = UIColor(red: 229/255, green: 252/255, blue: 255/255, alpha: 1)
-//        gridView.gridProperties.horizontalLineAttributes = [LineAttributes(color: UIColor(red: 222/255, green: 26/255, blue: 26/255, alpha: 1), divisor: 3, dashes: [36, 36], lineWidth: 5, roundedCap: false)]
-//        gridView.gridProperties.horizontalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [30, 55, 30, 30], lineWidth: 10, roundedCap: false)
-//        gridView.gridProperties.verticalLineAttributes = [LineAttributes(color: UIColor(red: 171/255, green: 218/255, blue: 252/255, alpha: 1), divisor: 3, dashes: [], lineWidth: 1, roundedCap: false)]
-//        gridView.gridProperties.verticalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [], lineWidth: 2, roundedCap: false)
+        gridView.gridProperties.scale = 10
+        gridView.gridProperties.lineColor = UIColor(red: 229/255, green: 252/255, blue: 255/255, alpha: 1)
+        gridView.gridProperties.horizontalLineAttributes = [LineAttributes(color: UIColor(red: 222/255, green: 26/255, blue: 26/255, alpha: 1), divisor: 3, dashes: [36, 36], lineWidth: 5, roundedCap: false)]
+        gridView.gridProperties.horizontalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [30, 30, 10, 30, 30], lineWidth: 10, roundedCap: false)
+        gridView.gridProperties.verticalLineAttributes = [LineAttributes(color: UIColor(red: 171/255, green: 218/255, blue: 252/255, alpha: 1), divisor: 3, dashes: [], lineWidth: 1, roundedCap: false)]
+        gridView.gridProperties.verticalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [], lineWidth: 20, roundedCap: false)
     }
 
     
@@ -88,6 +97,12 @@ class ViewController: UIViewController
     @IBAction func redrawButtonTapped(button: UIButton!)
     {
         gridView.gridView.setNeedsDisplay()
+    }
+    
+    @IBAction func stepperValueChanged(_: AnyObject!)
+    {
+        gridView.scrollView.zoomScale = CGFloat(pow(2, zoomStepper.value))
+        zoomLabel.text = "Zoom: \(gridView.scrollView.zoomScale) (\(zoomStepper.value))"
     }
 }
 
