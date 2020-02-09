30014dd4b686999500d64169de6cf08db9668620
Miloš Jagetić <milos.jagetic@gmail.com>
2020-02-09 10:49:40 +0100

    custom placement in example app

--- Example/MJGridView/ViewController.swift
+++ Example/MJGridView/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 import MJGridView
 
+extension OriginPlacement
+{
+    static var allCases: [OriginPlacement] = [.center,
+                                              .topCenter,
+                                              .topRight,
+                                              .centerRight,
+                                              .bottomRight,
+                                              .bottomCenter,
+                                              .bottomLeft,
+                                              .centerLeft,
+                                              .topLeft]
+}
+
 private extension String
 {
     static let placementIcons: String = "╋ ┳ ┓ ┫ ┛ ┻ ┖ ┣ ┏"
@@ -48,14 +61,14 @@ class ViewController: UIViewController
         spacingChanged(slider: spacingSlider)
         originPlacementChanged(slider: placementSlider)
         
-        gridView.backgroundColor = UIColor(red: 73/255, green: 72/255, blue: 80/255, alpha: 1)
+        gridView.backgroundColor = UIColor(red: 146/255, green: 144/255, blue: 160/255, alpha: 1)
 
         gridView.gridProperties.scale = 10
         gridView.gridProperties.lineColor = UIColor(red: 229/255, green: 252/255, blue: 255/255, alpha: 1)
         gridView.gridProperties.horizontalLineAttributes = [LineAttributes(color: UIColor(red: 222/255, green: 26/255, blue: 26/255, alpha: 1), divisor: 3, dashes: [36, 36], lineWidth: 5, roundedCap: false)]
-        gridView.gridProperties.horizontalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [30, 30, 10, 30, 30], lineWidth: 10, roundedCap: false)
+        gridView.gridProperties.horizontalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [30, 30, 10, 30, 30], lineWidth: 10, roundedCap: true)
         gridView.gridProperties.verticalLineAttributes = [LineAttributes(color: UIColor(red: 171/255, green: 218/255, blue: 252/255, alpha: 1), divisor: 3, dashes: [], lineWidth: 1, roundedCap: false)]
-        gridView.gridProperties.verticalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [], lineWidth: 20, roundedCap: false)
+        gridView.gridProperties.verticalAxisAttributes = LineAttributes(color: UIColor(red: 172/255, green: 172/255, blue: 222/255, alpha: 1), divisor: 0, dashes: [], lineWidth: 5, roundedCap: false)
     }
 
     
@@ -104,5 +117,13 @@ class ViewController: UIViewController
         gridView.scrollView.zoomScale = CGFloat(pow(2, zoomStepper.value))
         zoomLabel.text = "Zoom: \(gridView.scrollView.zoomScale) (\(zoomStepper.value))"
     }
+    
+    @IBAction func longPressAction(recognizer: UILongPressGestureRecognizer!)
+    {
+        let point: CGPoint = recognizer.location(in: gridView)
+        print(point)
+        gridView.gridProperties.originPlacement = .custom(point.x, point.y)
+    }
+
 }
 
